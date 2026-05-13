967f1eee7974a67a8dab938834e251ae4af2cd1f
VireArcanist <virelyn@outlook.de>
2026-05-13 14:00:29 +0200

    Add Book class

---
+++ Book.java
@@ -0,0 +1,26 @@
+public class Book extends Medium implements Lendable {
+    String author;
+    // Constructor to initialize the title, year, and author of the book.
+    Book(String title, int year, String author) {
+        super(title, year);
+        this.author = author;
+    }
+    // Method to lend the book, which sets the isLent status to true.
+    @Override
+    public void lend() {
+        if (isLent) {
+            System.out.println("You have lent: " + title);
+        } else {
+            System.out.println("Sorry, " + title + " is already lent out.");
+        }
+    }
+    // Method to return the book, which sets the isLent status to false.
+    @Override
+    public void returnItem() {
+        if (isLent) {
+            isLent = false;
+            System.out.println("You have returned: " + title);
+        }
+
+    }
+}   
